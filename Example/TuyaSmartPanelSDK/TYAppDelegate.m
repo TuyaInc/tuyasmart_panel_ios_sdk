b76aad1e2a53d2dae2dbc191a5918b38e484fea8
LiuShengtao <misaka@tuya.com>
2020-07-27 21:10:41 +0800

    update demo

--- Example/TuyaSmartPanelSDK/TYAppDelegate.m
+++ Example/TuyaSmartPanelSDK/TYAppDelegate.m
@@ -7,49 +7,48 @@
 //
 
 #import "TYAppDelegate.h"
-#import "TYPDeviceListViewController.h"
-#import "TYPLoginViewController.h"
+#import "TYDemoConfiguration.h"
+#import "TYDemoApplicationImpl.h"
 #import <TuyaSmartBaseKit/TuyaSmartBaseKit.h>
+#import <TuyaSmartBizCore/TuyaSmartBizCore.h>
+#import "TYSmartHomeDataProtocol.h"
+#import "TYPanelProtocol.h"
+#import "TYDemoSmartHomeManager.h"
 
 @implementation TYAppDelegate
-{
-    UINavigationController *_rootVc;
+
+- (TuyaSmartHome *)getCurrentHome {
+    return [TuyaSmartHome homeWithHomeId:[TYDemoSmartHomeManager sharedInstance].currentHomeModel.homeId];
+}
+
+- (void)gotoPanelControlDevice:(TuyaSmartDeviceModel *)device group:(TuyaSmartGroupModel *)group {
+
+    id<TYPanelProtocol> impl = [[TuyaSmartBizCore sharedInstance] serviceOfProtocol:@protocol(TYPanelProtocol)];
+    [impl presentPanelViewControllerWithDevice:device group:group initialProps:nil contextProps:nil completion:^(NSError * _Nullable error) {
+        
+    }];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [[TuyaSmartBizCore sharedInstance] registerService:@protocol(TYSmartHomeDataProtocol) withInstance:self];
+    [[TuyaSmartBizCore sharedInstance] registerRouteWithHandler:^BOOL(NSString * _Nonnull url, NSDictionary * _Nonnull raw) {
+        NSLog(@"路由: %@", url);
+        NSLog(@"参数: %@", raw);
+        return false;
+    }];
+    // Override point for customization after application launch.
+    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     // Override point for customization after application launch.
 #if DEBUG
-    [[TuyaSmartSDK sharedInstance] setDebugMode:YES];
+        [[TuyaSmartSDK sharedInstance] setDebugMode:YES];
 #endif
-    
-    [[TuyaSmartSDK sharedInstance] startWithAppKey:@"<#SDK_APPKEY#>" secretKey:@"<#SDK_APPSECRET#>"];
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:[TYPDeviceListViewController new]];
-    self.window.rootViewController = navigationController;
-    [self.window makeKeyAndVisible];
-    
-    _rootVc = navigationController;
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(showLoginVc)
-                                                 name:TuyaSmartUserNotificationUserSessionInvalid
-                                               object:nil];
-    
-    if (![TuyaSmartUser sharedInstance].isLogin) {
-        [self showLoginVc];
-    }
-    
-    return YES;
-}
+    [[TYDemoConfiguration sharedInstance] registService:@protocol(TYDemoPanelControlProtocol) withImpl:self];
+    TYDemoConfigModel *config = [[TYDemoConfigModel alloc] init];
+    config.appKey = @"<#SDK_APPKEY#>";
+    config.secretKey = @"<#SDK_APPSECRET#>";
 
-- (void)showLoginVc {
-    TYPLoginViewController *loginVc = [[TYPLoginViewController alloc] initWithNibName:nil bundle:nil];
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:loginVc];
-    nav.modalPresentationStyle = UIModalPresentationFullScreen;
-    [_rootVc presentViewController:nav animated:YES completion:nil];
+    return [[TYDemoApplicationImpl sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions config:config];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
